6a202e18e0db97611a2850ea13bacb7d24420883
igor <asilvaigor@gmail.com>
2020-03-09 13:05:00 +0100

    Fixing anims, changing layout

--- app/src/main/java/com/nupanca/MainActivity.kt
+++ app/src/main/java/com/nupanca/MainActivity.kt
@@ -13,18 +13,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
-        supportActionBar?.setDisplayShowTitleEnabled(false)
-
-//        toolbar.setNavigationOnClickListener { view ->
-//            Snackbar.make(view, "Replace with this", Snackbar.LENGTH_LONG)
-//                .setAction("Action", null).show()
-//        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-//        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
     }
 }
