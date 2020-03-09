2a132830ff2c90df46db0d9a25f5cca5036681be
igor <asilvaigor@gmail.com>
2020-03-09 11:21:39 +0100

    Changing fragments names

--- app/src/main/java/com/nupanca/MainFragment.kt
+++ app/src/main/java/com/nupanca/MainFragment.kt
@@ -5,31 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.snackbar.Snackbar
-import kotlinx.android.synthetic.main.fragment_first.*
+import kotlinx.android.synthetic.main.fragment_main.*
 
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class MainFragment : Fragment() {
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         total_amount_layout.setOnClickListener {
-            Snackbar.make(it, "Replace with this", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+            findNavController().navigate(R.id.action_MainFragment_to_TransfersFragment)
         }
 
         goals_layout.setOnClickListener {
@@ -40,10 +38,6 @@ class FirstFragment : Fragment() {
         control_layout.setOnClickListener {
             Snackbar.make(it, "Replace with this", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
-        }
-
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
     }
 }
