730dde5a2bdae3467517a19b08ab46531524fdbc
Aloysio <aloysiogl@gmail.com>
2020-03-13 20:00:23 +0100

    Setting screen to not rotate

--- app/src/main/java/com/nupanca/MainActivity.kt
+++ app/src/main/java/com/nupanca/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.nupanca
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
@@ -7,6 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LOCKED;
     }
 
     override fun onBackPressed() {
