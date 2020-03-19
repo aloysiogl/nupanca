b4c6205af10d7c5d7aaf5972f6d7021de7b2a1c5
igor <asilvaigor@gmail.com>
2020-03-19 14:38:37 -0300

    Starting goal updating

--- app/src/main/java/com/nupanca/GoalsListFragment.kt
+++ app/src/main/java/com/nupanca/GoalsListFragment.kt
@@ -16,8 +16,11 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.database.*
+import com.nupanca.db.AccountInfo
 import com.nupanca.db.Goal
 import kotlinx.android.synthetic.main.fragment_goals_list.*
+import java.text.SimpleDateFormat
+import java.util.*
 import kotlin.collections.HashMap
 
 
@@ -26,16 +29,17 @@ import kotlin.collections.HashMap
  */
 class GoalsListFragment : BaseFragment() {
     var goalAdapter: GoalAdapter? = null
-    val goals =  HashMap<String?, Goal>()
-    private lateinit var database: DatabaseReference
+    val goals = HashMap<String?, Goal>()
+    var accountInfo = AccountInfo()
+    var db = FirebaseDatabase.getInstance()
+    var goalListRef = db.getReference("goal_list")
+    var accontInfoRef = db.getReference("account_info")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        database = FirebaseDatabase.getInstance().getReference("goal_list")
-
-        val childEventListener = object : ChildEventListener {
+        goalListRef.addChildEventListener(object : ChildEventListener {
             override fun onCancelled(p0: DatabaseError) {
             }
 
@@ -49,15 +53,24 @@ class GoalsListFragment : BaseFragment() {
             }
 
             override fun onChildAdded(dataSnapshot: DataSnapshot, previousChildName: String?) {
-                    val newGoal = Goal.fromMap(dataSnapshot.value as HashMap<String, Any>)
-                    goals[newGoal.key] = newGoal
-                    val multi = goals.values.toMutableList()
+                val newGoal = Goal.fromMap(dataSnapshot.value as HashMap<String, Any>)
+                goals[newGoal.key] = newGoal
+                val multi = goals.values.toMutableList()
 
-                    if (goals_list != null)
-                        goals_list.adapter = GoalAdapter(multi)
+                if (goals_list != null)
+                    goals_list.adapter = GoalAdapter(multi)
             }
-        }
-        database.addChildEventListener(childEventListener)
+        })
+
+        accontInfoRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                accountInfo = AccountInfo.fromMap(dataSnapshot.value as HashMap<String, Any>)
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+
+            }
+        })
 
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_goals_list, container, false)
@@ -94,7 +107,7 @@ class GoalsListFragment : BaseFragment() {
             dialog?.show()
 
             val textView = dialog?.findViewById<View>(android.R.id.message) as TextView
-            textView.typeface  = context?.let { it1 ->
+            textView.typeface = context?.let { it1 ->
                 ResourcesCompat.getFont(
                     it1,
                     R.font.keep_calm_w01_book
@@ -117,5 +130,18 @@ class GoalsListFragment : BaseFragment() {
     override fun onBackPressed(): Boolean {
         button_return.performClick()
         return true
+    }
+
+    fun updateGoals() {
+        val totalAmount = accountInfo.savingsBalance
+        val savingsPerMonth = (accountInfo.savings30Days + accountInfo.savingsPlan) / 2
+        val prioritiesValsPerMonth = doubleArrayOf(0.0, 0.0, 0.0, 0.0, 0.0)
+
+        for (k in goals) {
+            var startDate = k.value.beginDate?.let { Date(it) }
+            var endDate = Date(k.value.endDate)
+            val monthsToFinish = endDate.compareTo(startDate)  // TODO: Make this be an int
+            prioritiesValsPerMonth[k.value.priority] += k.value.totalAmount / monthsToFinish
+        }
     }
 }
